78f251b2eaf62cc0acd9cc34a17eee506094962c
celine_co <celine.cossez@jasparo.fr>
2025-10-31 17:39:39 +0100

    twilio file & gitignore

---
+++ twilio.ts
@@ -0,0 +1,17 @@
+const twilio = require("twilio"); // Or, for ESM: import twilio from "twilio";
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+async function createMessage() {
+  const message = await client.messages.create({
+    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+    from: "+15017122661",
+    to: "+15558675310",
+  });
+
+  console.log(message.body);
+}
+
+createMessage();
