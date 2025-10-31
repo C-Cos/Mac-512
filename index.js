9323bcc383804c1717cf8b20fc2a65517d6ae5d8
celine_co <celine.cossez@gmail.com>
2025-10-31 16:44:07 +0100

    add typescript + refacto index to add iphone search

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const cheerio = require("cheerio");
 
-const url = "https://www.apple.com/fr/shop/refurbished/mac/macbook-air-16-go";
+const urlMacBook =
+  "https://www.apple.com/fr/shop/refurbished/mac/macbook-air-16-go";
+const urlIphone = "https://www.apple.com/fr/shop/refurbished/iphone";
 
-(async () => {
-  const $ = await cheerio.fromURL(url);
+export const searchMacBookAir = async () => {
+  const $ = await cheerio.fromURL(urlMacBook);
   let items = $(".rf-refurb-category-grid-no-js h3 a")
     .map(function () {
       return $(this).text();
     })
     .toArray();
   return items.filter((v) => v.includes("Air") && v.includes("13"));
-})();
+};
+
+export const searchIphone16 = async () => {
+  const $ = await cheerio.fromURL(urlIphone);
+  let items = $(".rf-refurb-category-grid-no-js h3 a")
+    .map(function () {
+      return $(this).text();
+    })
+    .toArray();
+  console.log(items);
+  return items.filter((v) => v.includes("16"));
+};
