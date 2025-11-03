478c44c78d2205ebdf9d19ccbb464b391fb76f79
celine_co <celine.cossez@gmail.com>
2025-11-03 20:16:22 +0100

    adding errors

--- index.ts
+++ index.ts
@@ -4,15 +4,23 @@ import { searchMacBookAir } from "#search";
 import isEmpty from "#utils";
 
 const startJob = async () => {
-  searchIphone16().then((res) => {
-    if (!isEmpty(res))
-      createMessage("There is some refurbished Iphone 16 available !");
-  });
+  searchIphone16()
+    .then((res) => {
+      if (!isEmpty(res))
+        createMessage("There is some refurbished Iphone 16 available !");
+    })
+    .catch((e) => {
+      throw new Error("Something went wrong looking for Iphones", e);
+    });
 
-  searchMacBookAir().then((res) => {
-    if (!isEmpty(res))
-      createMessage("There is some refurbished MacBook Air available !");
-  });
+  searchMacBookAir()
+    .then((res) => {
+      if (!isEmpty(res))
+        createMessage("There is some refurbished MacBook Air available !");
+    })
+    .catch((e) => {
+      throw new Error("Something went wrong looking for Macbooks", e);
+    });
 };
 
 startJob();
